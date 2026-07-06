c4559b9701546bf53253f047b961e58cea37a15a
Ahmad Faraz <72246950+Frzx@users.noreply.github.com>
2026-07-06 12:50:52 +0530

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -1,1 +1,3 @@
 curl -i http://localhost:3000/mcp -H "Authorization: Bearer <CLIENT_TOKEN>" -H "Accept: application/json, text/event-stream" -H "Content-Type: application/json" -d "{\"jsonrpc\":\"2.0\",\"id\":\"1\",\"method\":\"initialize\",\"params\":{\"protocolVersion\":\"2025-11-25\",\"capabilities\":{},\"clientInfo\":{\"name\":\"test\",\"version\":\"1.0\"}}}"
+
+docker exec -it <container> python -c "import socket; s=socket.create_connection(('doris.toolkitdoris.ppe.euw.azure.tesco.org',9030),10); print('ok')"
